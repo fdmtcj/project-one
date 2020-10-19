e88496024db2ca9f290425ce4abc1d4e1b98254d
Jakub Rej <jrej@edu.cdv.pl>
2020-10-19 17:52:23 +0200

    first commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,20 @@
+package com.kodilla;
+
+public class Calculator {
+
+    public void addition() {
+        System.out.println("result=0");
+
+    }
+    public void substraction(){
+        System.out.println("result=0");
+
+    }
+
+    public static void main(String[]args){
+        Calculator calculator= new Calculator();
+        calculator.addition();
+    }
+}
+
+
