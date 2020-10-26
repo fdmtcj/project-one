f48939a52caa30cacbd6756b80a2fc19f87c8c1b
Jakub Rej <jrej@edu.cdv.pl>
2020-10-26 17:41:01 +0100

    calculator fix

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -2,24 +2,26 @@ package com.kodilla;
 
 public class Calculator {
 
-    public void addition(int a, int b) {
-        int result= a+b ;
-
-     System.out.println("Result= "+ result);
-
-    }
-    public void substraction(){
-        System.out.println("result=0");
-
+    public int addition(int a, int b) {
+        return a + b;
     }
 
-    public static void main(String[]args){
-        Calculator calculator= new Calculator();
-        calculator.addition(2,5);
-        calculator.addition(4,6);
+    public int substraction(int a, int b) {
+        return a - b;
     }
 
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
 
+        int addition = calculator.addition(2, 5);
+        System.out.println(addition);
+        int substraction = calculator.substraction(9, 3);
+        System.out.println(substraction);
+
+    }
 }
+
+
+
 
 
